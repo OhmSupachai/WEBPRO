fcbd16a9dbc64bf5c5cb017735d5091553ef5e5e
Ohmkung <henryohohm@gmail.com>
2019-11-11 19:36:58 +0700

    adminSQL

---
+++ admin.sql
@@ -0,0 +1,4 @@
+insert into admin (admin_id, admin_name, admin_email) values (1, 'Tanhya Filyukov', 'tfilyukov0@mediafire.com');
+insert into admin (admin_id, admin_name, admin_email) values (2, 'Natal Starrs', 'nstarrs1@deviantart.com');
+insert into admin (admin_id, admin_name, admin_email) values (3, 'Karilynn Keen', 'kkeen2@livejournal.com');
+insert into admin (admin_id, admin_name, admin_email) values (4, 'Edan Tape', 'etape3@rambler.ru');
