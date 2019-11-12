d3fab35430259aa474727b902bc2f33a257faf54
Ohmkung <henryohohm@gmail.com>
2019-11-12 20:00:11 +0700

    DB ALL

--- student.sql
+++
@@ -1,4 +0,0 @@
-insert into student (student_id, student_name, student_email, user_user_id) values (1, 'Alford Coxhead', 'acoxhead0@meetup.com', 1);
-insert into student (student_id, student_name, student_email, user_user_id) values (2, 'Silas Melin', 'smelin1@wikia.com', 2);
-insert into student (student_id, student_name, student_email, user_user_id) values (3, 'Mathe Doeg', 'mdoeg2@ucoz.ru', 3);
-insert into student (student_id, student_name, student_email, user_user_id) values (4, 'Hedwiga Thoumas', 'hthoumas3@tinypic.com', 4);
--- teacher.sql
+++
@@ -1,1 +0,0 @@
-insert into teacher (teacher_id, teacher_name, teacher_email, user_user_id) values (5, 'Dewitt McGillecole', 'dmcgillecole0@earthlink.net', 5);
